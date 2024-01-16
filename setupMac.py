27e90bed0aa0d507b662cb2ca09775778899432f
chapmanwilliam <wchapman10@gmail.com>
2024-01-16 14:54:53 +0000

    ssetup Mac

---
+++ setupMac.py
@@ -0,0 +1,17 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+build_options = {'packages': [], 'excludes': [], 'include_files': ['Resources/'], 'includes': ['fitz.mupdf']}
+
+base = 'gui'
+
+executables = [
+    Executable('Display.py', base=base, target_name = 'Autobookmark')
+]
+
+setup(name='Autobookmark',
+      version = '1.0',
+      description = 'Bookmarks',
+      options = {'build_exe': build_options},
+      executables = executables)
